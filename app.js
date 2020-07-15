4a8d4c157ad86c9cd4edf7ad92b9d5be74998683
Alpha Martinez <alphamartinez@Alphas-MacBook-Pro.local>
2020-07-15 17:45:52 -0400

    [loops] for loop

--- app.js
+++ app.js
@@ -46,3 +46,24 @@ while (mingo < day){
     mingo += 1;//
 }
 
+//for loop
+
+var mon = 12;
+var wed = 30;
+
+for (var i = mon; i < wed; i++) {
+    console.log(i);
+}
+
+
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+//teams
+for (var i = 0; i < teams.length; i++) {
+    //console.log(teams[i]) is saying teams starting at 0, print out so forth but don't do this tho
+    var single = teams[i];
+    console.log(single);
+}
+
