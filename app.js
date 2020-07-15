96f88f12595f896acbfdfb8d65bfac9d792493ce
Alpha Martinez <alphamartinez@Alphas-MacBook-Pro.local>
2020-07-15 17:08:20 -0400

    [arrays] Add favorite teams

---
+++ app.js
@@ -0,0 +1,11 @@
+var name = "Alpha";
+console.log(name);
+
+var myLocation = "New York";
+console.log(myLocation);
+
+var favoriteNumber = 5;
+console.log(favoriteNumber);
+
+var teams = ["Lakers", "Warriors", "Blazers", "Nets"];
+console.log(teams);
